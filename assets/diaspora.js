2096036ff4960900b0246f2cc0c0a13caf0be53b
Amy Boudsady <aeimskei@gmail.com>
2019-02-03 11:03:42 -0800

    Try again, menu link open in same window

--- assets/diaspora.js
+++ assets/diaspora.js
@@ -20,7 +20,7 @@ var ios = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
 if(ios) {
   $('a').on('click touchend', function() { 
     var link = $(this).attr('href');   
-    window.open(link);
+    window.open(link, '_self');
     return false;
   });
 }
